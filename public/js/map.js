d97b175b07df816049f7bb7bfb2b5836a1fd33a6
Joonas88 <56264853+Joonas88@users.noreply.github.com>
2020-12-03 21:03:08 +0200

    Added an unfunctioning button to the range icons, reafactored a function and made it obsolete, cleaned up the layout of the map and moved buttons around.

--- public/js/map.js
+++ public/js/map.js
@@ -51,18 +51,10 @@ function error(err) {
  *
  */
 const searchLayer = L.layerGroup().addTo(map);
-//... adding data in searchLayer ...
-map.addControl( new L.Control.Search({
-    position:'topright',
-    layer: searchLayer,
-    initial: false,
-    zoom: 15,
-    marker: false
-}) );
-//searchLayer is a L.LayerGroup contains searched markers
 
 L.control.scale().addTo(map);
 
+map.zoomControl.setPosition('topright');
 /**
  *
  */
@@ -99,34 +91,42 @@ function addMarker(crd, teksti) {
  */
 const legend = L.control({position: 'topleft'});
 legend.onAdd = function (map) {
-    const div = L.DomUtil.create('div', 'info legend');
+    const div = L.DomUtil.create('div', 'area legend');
     div.innerHTML = '<select name="valinta" id="valinta">\n' +
-        '    <option value="empty" id="empty">Valitse maakunta tästä:</option>\n' +
-        '    <option value="ahvenanmaa" id="ahvenanmaa">Ahvenanmaa</option>\n' +
-        '    <option value="etela-karjala" id="etela-karjala">Etelä-Karjala</option>\n' +
-        '    <option value="etela-pohjanmaa" id="etela-pohjanmaa">Etelä-Pohjanmaa</option>\n' +
-        '    <option value="etela-savo" id="etela-savo">Etelä-Savo</option>\n' +
-        '    <option value="kainuu" id="kainuu">Kainuu</option>\n' +
-        '    <option value="kanta-hame" id="kanta-hame">Kanta-Häme</option>\n' +
-        '    <option value="keski-pohjanmaa" id="keski-pohjanmaa">Keski-Pohjanmaa</option>\n' +
-        '    <option value="keski-suomi" id="keski-suomi">Keski-Suomi</option>\n' +
-        '    <option value="kymenlaakso" id="kymenlaakso">Kymenlaakso</option>\n' +
-        '    <option value="lappi" id="lappi">Lappi</option>\n' +
-        '    <option value="pirkanmaa" id="pirkanmaa">Pirkanmaa</option>\n' +
-        '    <option value="pohjois-karjala" id="pohjois-karjala">Pohjois-Karjala</option>\n' +
-        '    <option value="pohjois-pohjanmaa" id="pohjois-pohjanmaa">Pohjois-Pohjanmaa</option>\n' +
-        '    <option value="pohjois-savo" id="pohjois-savo">Pohjois-Savo</option>\n' +
-        '    <option value="paijat-hame" id="paijat-hame">Päijät-Häme</option>\n' +
-        '    <option value="satakunta" id="satakunta">Satakunta</option>\n' +
-        '    <option value="uusimaa" id="uusimaa">Uusimaa</option>\n' +
-        '    <option value="varsinais-suomi" id="varsinais-suomi">Varsinais-Suomi</option>\n' +
-        '    <option value="kokosuomi" id="kokosuomi">Kaikki radat</option>\n' +
+        '    <option value="empty" id="empty">Maakunnat:</option>\n' +
+        '    <option value="Ahvenanmaa" id="ahvenanmaa">Ahvenanmaa</option>\n' +
+        '    <option value="Etelä-Karjala" id="etela-karjala">Etelä-Karjala</option>\n' +
+        '    <option value="Etelä-Pohjanmaa" id="etela-pohjanmaa">Etelä-Pohjanmaa</option>\n' +
+        '    <option value="Etelä-Savo" id="etela-savo">Etelä-Savo</option>\n' +
+        '    <option value="Kainuu" id="kainuu">Kainuu</option>\n' +
+        '    <option value="Kanta-Häme" id="kanta-hame">Kanta-Häme</option>\n' +
+        '    <option value="Keski-Pohjanmaa" id="keski-pohjanmaa">Keski-Pohjanmaa</option>\n' +
+        '    <option value="Keski-Suomi" id="keski-suomi">Keski-Suomi</option>\n' +
+        '    <option value="Kymenlaakso" id="kymenlaakso">Kymenlaakso</option>\n' +
+        '    <option value="Lappi" id="lappi">Lappi</option>\n' +
+        '    <option value="Pirkanmaa" id="pirkanmaa">Pirkanmaa</option>\n' +
+        '    <option value="Pohjois-Karjala" id="pohjois-karjala">Pohjois-Karjala</option>\n' +
+        '    <option value="Pohjois-Pohjanmaa" id="pohjois-pohjanmaa">Pohjois-Pohjanmaa</option>\n' +
+        '    <option value="Pohjois-Savo" id="pohjois-savo">Pohjois-Savo</option>\n' +
+        '    <option value="Päijät-Häme" id="paijat-hame">Päijät-Häme</option>\n' +
+        '    <option value="Satakunta" id="satakunta">Satakunta</option>\n' +
+        '    <option value="Uusimaa" id="uusimaa">Uusimaa</option>\n' +
+        '    <option value="Varsinais-Suomi" id="varsinais-suomi">Varsinais-Suomi</option>\n' +
+        '    <option value="kaikki" id="kokosuomi">Kaikki radat</option>\n' +
         '</select>' +
-        '<br><input onclick="trackSearch()" type="button" value="Näytä radat" id="submit">';
+        '<br><input onclick="makeQuery()" type="button" value="Näytä radat" id="submit">';
     div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
     return div;
 };
 legend.addTo(map);
+
+map.addControl( new L.Control.Search({
+    position:'topleft',
+    layer: searchLayer,
+    initial: false,
+    zoom: 15,
+    marker: false
+}) );
 
 /**
  *
@@ -140,84 +140,14 @@ myLocation.onAdd = function (map) {
 };
 myLocation.addTo(map);
 
-const dropdownAreas = document.getElementById('valinta');
-/**
- *
- */
-function trackSearch() { //Funktiolla määritellään mitä tapahtuu hakunappia painettaessa
-let area;
-    switch (dropdownAreas.selectedIndex) { //Switch-case määrittää mikä pudostusvalikon atribuutti on käytössä ja default päivittä kartan
-        case 1:
-            area = 'Ahvenanmaa';
-            break;
-        case 2:
-            area = 'Etelä-Karjala';
-            break;
-        case 3:
-            area = 'Etelä-Pohjanmaa';
-            break;
-        case 4:
-            area = 'Etelä-Savo';
-            break;
-        case 5:
-            area = 'Kainuu';
-            break;
-        case 6:
-            area = 'Kanta-Häme';
-            break;
-        case 7:
-            area = 'Keski-Pohjanmaa';
-            break;
-        case 8:
-            area = 'Keski-Suomi';
-            break;
-        case 9:
-            area = 'Kymenlaakso';
-            break;
-        case 10:
-            area = 'Lappi';
-            break;
-        case 11:
-            area = 'Pirkanmaa';
-            break;
-        case 12:
-            area = 'Pohjois-Karjala';
-            break;
-        case 13:
-            area = 'Pohjois-Pohjanmaa';
-            break;
-        case 14:
-            area = 'Pohjois-Savo';
-            break;
-        case 15:
-            area = 'Päijät-Häme';
-            break;
-        case 16:
-            area = 'Satakunta';
-            break;
-        case 17:
-            area = 'Uusimaa';
-            break;
-        case 18:
-            area = 'Varsinais-Suomi';
-            break;
-        case 19:
-            area = 'kaikki';
-            break;
-        default:
-            alert('Valitse jokin näytettävä arvo.')
-            break;
-    }
-    makeQuery(area)
-}
-
 /**
  * makeQuery-function is called when there is a need to go into the servers database. The function accesses the server
  * trough an xmlhttp-request and passes data to the server, in this case the chosen are where the frisbeegolf courses
  * are searched for.
  * @param area
  */
-function makeQuery(area) {
+function makeQuery() {
+    const area = document.getElementById('valinta').value;
     searchLayer.clearLayers();
     let crd, teksti;
     let xmlhttp = new XMLHttpRequest();
@@ -231,7 +161,7 @@ function makeQuery(area) {
                 if (json.rows[i].latitude!==0&&json.rows[i].longitude!==0){
                     crd = {latitude: json.rows[i].latitude, longitude: json.rows[i].longitude};
                     teksti = json.rows[i].location_name;
-                    const trackMarker = L.marker([crd.latitude, crd.longitude], {title: teksti, icon: basketIcon}).bindPopup(teksti+'<br>'+crd.latitude+' '+crd.longitude).openPopup().on('click', function () {
+                    const trackMarker = L.marker([crd.latitude, crd.longitude], {title: teksti, icon: basketIcon}).bindPopup(teksti+'<br><input onclick="" type="button" value="Pelaa tämä" id="submit">').openPopup().on('click', function () {
                     });
                     searchLayer.addLayer(trackMarker);
                 }
